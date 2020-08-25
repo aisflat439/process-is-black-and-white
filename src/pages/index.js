eb1dcab3b7ab94486be1bcdb5c7271cd0ad28a48
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-08-25 10:55:59 -0600

    feat make twitter link readable

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>Harras Devin to build something decent for this website instead of this by messaging @process_is on twitter..</p>
+    <p>Harras Devin to build something decent for this website instead of this by messaging <a href="https://twitter.com/process_is" target="_blank" rel="noopener">@process_is</a> on twitter..</p>
     <br />
     <div style={{ maxWidth: `70vw`, marginBottom: `1.45rem` }}>
       <Episodes />
