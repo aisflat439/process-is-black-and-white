d97278381af6914676fa160a13b47b77fa011d67
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-09-21 10:51:30 -0600

    feat: add a second chevron

--- src/pages/books.js
+++ src/pages/books.js
@@ -28,12 +28,17 @@ const SecondPage = () => (
     <h2>Here's the books we read and links to get them</h2>
     {BOOKS.map(book => (
       <div style={{
+        padding: '.5rem',
         margin: '1rem',
-        padding: '2rem',
-        border: '1px solid black'
+        border: '2px solid black'
       }}>
-        <h3>{book.title} &nbsp;&nbsp;&nbsp;<small>{book.author}</small></h3>
-        <a href={book.link} target="_blank" rel="noreferrer">Buy it here</a>
+        <div style={{
+          padding: '2rem',
+          border: '2px solid black'
+        }}>
+          <h3>{book.title} &nbsp;&nbsp;&nbsp;<small>{book.author}</small></h3>
+          <a href={book.link} target="_blank" rel="noreferrer">Buy it here</a>
+        </div>
       </div>
     ))}
     <Link to="/">Go back to the homepage</Link>
