f2108e8bc83bad28547c422604ef1efc45531263
Devin Fitzsimons <fitzsimonsdevin@gmail.com>
2020-10-14 14:00:02 -0600

    feat: begin contact us

---
+++ src/components/contact-us.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const ContactUs = () => {
+
+  return (
+    <div style={{
+      backgroundColor: 'black',
+      color: "white",
+      padding: '.5rem',
+      display: 'flex',
+    }}>
+      <div
+        style={{
+          flex: 1,
+          border: `.334rem solid white`,
+          padding: `.5rem`,
+        }}
+      >
+        CONTACT
+      </div>
+    </div >
+  )
+};
+
+export default ContactUs
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,45 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Episodes from "../components/episodes"
 import SEO from "../components/seo"
 import Subscribe from "../components/subscribe"
+import ContactUs from "../components/contact-us"
+
+const ContactContainer = styled.div`
+  display: flex;
+  > p {
+    flex: 5;
+  }
+  > div {
+    flex: 3;
+    display: flex;
+  }
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <main>
+    <main >
       <article>
         <h1 style={{ fontSize: `1rem`, marginBottom: `1.45rem` }}>A weekly show following the entrepreneurship journeys of two friends.</h1>
-        <p>If you'd enjoy sharing the journey of two friends building businesses, this show is for you. Vernon and Devin found friendship through a shared love of Philadelphia sports. As their friendship evolved and grew over time they began to discuss life, family, food and business. They both began to look to start a side business in 2020 and realized they were at a similar stage. Over the course of a few text messages and conversations they decided to start a book club to learn along together. Each week, they discuss a few chapters from a book, give updates about steps they've taken in the journey from wantrepreneur to entrepreneur.</p>
+        <ContactContainer>
+          <p>If you'd enjoy sharing the journey of two friends building businesses, this show is for you. Vernon and Devin found friendship through a shared love of Philadelphia sports. As their friendship evolved and grew over time they began to discuss life, family, food and business. They both began to look to start a side business in 2020 and realized they were at a similar stage. Over the course of a few text messages and conversations they decided to start a book club to learn along together. Each week, they discuss a few chapters from a book, give updates about steps they've taken in the journey from wantrepreneur to entrepreneur.</p>
+          <ContactUs />
+        </ContactContainer>
         <h4>Show format</h4>
         <p>Generally speaking, each episode covers each of us giving updates about our week, some light sports chatter, and reflections about the book their reading together. <Link to="/books/">The books</Link> are suggested in turn and are most frequently business books. That said, both of us believe that books are a window into wisdom, so we read what we're curious about, and grow our knowledge base together. We both firmly believe that the path to success involves dealing directly with our cultural differences and similarities. We share the view that the path to freedom involves sharing and helping each other, no matter if we're black or white, in tech or brick and mortar, or any other differences. The lessons of being a good person and entrepreneur apply across cultural and socioeconomic divides, and when we apply those lessons together, we're all better off.</p>
       </article>
     </main>
-    <Subscribe />
+    <aside>
+      <Subscribe />
+    </aside>
     <div style={{ maxWidth: `70vw`, margin: `auto`, marginBottom: `1.45rem` }}>
       <Episodes />
     </div>
-  </Layout>
+  </Layout >
 )
 
 export default IndexPage
