9b72b768fdc401343aaaa1d99f8b6715a9af2ee7
Devin <fitzsimonsdevin@gmail.com>
2021-09-28 17:39:54 -0600

    add in new episode

--- src/components/episodes.js
+++ src/components/episodes.js
@@ -1,18 +1,18 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
-const makePath = (title) => title
-      .replace(/[^a-zA-Z0-9 ]/g, "")
-      .replace(/ /g, '-')
-      .replace(/---/g, '-')
-      .replace(/--/g, '-')
-      .toLowerCase();
-
+const makePath = title =>
+  title
+    .replace(/[^a-zA-Z0-9 ]/g, "")
+    .replace(/ /g, "-")
+    .replace(/---/g, "-")
+    .replace(/--/g, "-")
+    .toLowerCase()
 
 const Episodes = () => {
   const data = useStaticQuery(graphql`
     query MyQuery {
-      allFeedPodcastEpisode(sort: {order: DESC, fields: isoDate}, limit: 10) {
+      allFeedPodcastEpisode(sort: { order: DESC, fields: isoDate }, limit: 10) {
         edges {
           node {
             id
@@ -28,34 +28,43 @@ const Episodes = () => {
     }
   `)
 
-  const episodes = data.allFeedPodcastEpisode.edges; 
+  const episodes = data.allFeedPodcastEpisode.edges
 
   return (
     <div style={{ padding: `.5rem`, marginTop: `1.45rem` }}>
       {episodes.map(({ node: episode }) => (
-        <div 
-          key={episode.title.replace(' ', '-')} 
-          data-testId={episode.title.replace(' ', '-')} 
-          style={{ border: `2px solid black`, padding: `.5rem`, marginBottom: `1.45rem` }}
+        <div
+          key={episode.title.replace(" ", "-")}
+          data-testId={episode.title.replace(" ", "-")}
+          style={{
+            border: `2px solid black`,
+            padding: `.5rem`,
+            marginBottom: `1.45rem`,
+          }}
         >
           <div style={{ border: `2px solid black`, padding: `1.5rem` }}>
-            <h2 className="base-font" style={{ marginTop: `1rem` }}><span style={{ color: `black`, textDecoration: `none` }}>{episode.title}</span></h2>
-            <div style={{ marginBottom: '10px' }}><Link to={`podcast/${makePath(episode.title)}`}>Listen here</Link> </div>
+            <h2 className="base-font" style={{ marginTop: `1rem` }}>
+              <span style={{ color: `black`, textDecoration: `none` }}>
+                {episode.title}
+              </span>
+            </h2>
+            <div style={{ marginBottom: "10px" }}>
+              <Link to={`podcast/${makePath(episode.title)}`}>Listen here</Link>{" "}
+            </div>
             <div dangerouslySetInnerHTML={{ __html: episode.content }} />
           </div>
         </div>
-      ))
-      }
+      ))}
       {/* <div>
         <article>
-          <p>Chapters 1-2-3 of The Lean Startup</p>
-          <p>The guys discuss what they think of the USA losing to France. Is JoJo and American basketball player? Is Luka? Vernon and Devin are both moving forward in their businesses, figuring out what's next. Along with discussion and reflection on this weeks chapters. Join us, and tell a friend.</p>
+          <p>Chapters 11-12-13 of Storybrand</p>
+          <p>This week we discuss how we react to the closing of Storybrand. What are we taking away, and how will we think about this book going forward. What they heck is an above the fold message? Next week we finish up Storybrand with the marketing playbook. Thanks for listening along!</p>
           <h3>Links:</h3>
-          <p>Our current book  <a href="https://amzn.to/3hhs80s">The Lean Startup</a></p>
+          <p>Our current book  <a href="https://www.amazon.com/Building-StoryBrand-Clarify-Message-Customers/dp/0718033329">Storybrand</a></p>
         </article>
       </div> */}
-    </div >
+    </div>
   )
-};
+}
 
 export default Episodes
